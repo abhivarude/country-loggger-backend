3a96a641dea9ca598000bb030434c73277945c0e
Abhijeet Haridas Varude <53347711+abhivarude@users.noreply.github.com>
2021-10-18 06:48:41 +0530

    Add files via upload

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,39 +1,106 @@
-const joi=require("joi");
+const express=require("express");
+const router=express.Router();
+const bcrypt=require("bcrypt");
+const {signupvalidation,loginvalidation} =require("./validation");
+var {signupSchema,queryschema,calenderschema} =require("../moduler/urlschema");
+router.post("/signup",async (req,res)=>{
 
-const sigupvalidation=(data)=>{
+try{
 
-const validationschema=joi.object(
+
+    const{error}=signupvalidation(req.body);
+    if(error){
+    res.status(400).send({msg:error.details[0].message})
+    }
+    
+    
+    
+    var data=await signupSchema.findOne({Email:req.body.Email});
+    if(data)
     {
-       
+        res.status(201).json("Email alerady exists");
+    }
+    else{
 
-        name:joi.string().min(5).required(),
-        email:joi.string().min(6).email().required(),
-        password:joi.string().min(5).max(12).required()
+        const salt=await bcrypt.genSalt(10);
+        req.body.password=await bcrypt.hash(req.body.password,salt);
+        signup= new signupSchema ({
+            cname:req.body.cname,
+            Email:req.body.Email,
+            password:req.body.password
+
+        })
+
+
+        await signup.save();
+ res.status(200).json(signup);
 
     }
-)
 
-return validationschema.validate(data)
 
 }
+catch(error){
+    console.log(error);
+}
 
-
-const loginvalidation=(data)=>{
-
-const lvalidationschema=joi.object({
-
-
-    name:joi.string().min(5).required(),
-    email:joi.string().email().required(),
-    data:joi.string().min(5).required()
 
 })
 
-return lvalidationschema.validate(data);
 
+
+router.post("/login",async (req,res)=>{
+    console.log(req.body)
+const {error}=loginvalidation(req.body);
+if(error)
+{
+    return res.status(400).send({msg:error.details[0].message})
 }
 
 
+const logindata=await signupSchema.findOne({Email:req.body.Email})
+if(!logindata)
+{
+  return  res.status(401).send({msg:'Email dosent exists'})
+}
+console.log(req.body.password);
+console.log(logindata.password);
+const isValid=await bcrypt.compare(req.body.password,logindata.password)
+
+if(!isValid)
+{
+    return res.status(402).send("password dosen't match");
+}
+else{return res.status(200).send("verified user")
+}
+
+})
 
 
-module.exports={sigupvalidation,loginvalidation}
+
+router.post("/query",async(req,res)=>{
+
+
+query=queryschema({
+    querytitle:req.body.querytitle,
+    description:req.body.description
+
+    
+
+})
+console.log(req.body);
+await query.save();
+res.status(200).json(query);
+
+})
+
+router.post("/calender",async(req,res)=>{
+
+
+const data=await calenderschema.findOne(req.body);
+res.status(200).send(data)
+
+
+
+})
+
+module.exports=router;
